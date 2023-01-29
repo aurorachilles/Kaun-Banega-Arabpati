7dbbec83684c20ea1233b3985c4d0baf3eba9d65
Vishal Pattanaik <vishal.pattanaik05@gmail.com>
2023-01-30 02:58:39 +0530

    new file:   build.xml 	new file:   build/classes/.netbeans_automatic_build 	new file:   build/classes/.netbeans_update_resources 	new file:   build/classes/images/Main_back.png 	new file:   build/classes/kaun_banega_neelpati/Kaun_Banega_Neelpati.class 	new file:   build/classes/main$1.class 	new file:   build/classes/main$2.class 	new file:   build/classes/main$3.class 	new file:   build/classes/main.class 	new file:   build/classes/main.form 	new file:   manifest.mf 	new file:   nbproject/build-impl.xml 	new file:   nbproject/genfiles.properties 	new file:   nbproject/private/private.properties 	new file:   nbproject/private/private.xml 	new file:   nbproject/project.properties 	new file:   nbproject/project.xml 	new file:   src/images/Main_back.png 	new file:   src/kaun_banega_neelpati/Kaun_Banega_Neelpati.java 	new file:   src/main.form 	new file:   src/main.java

---
+++ src/kaun_banega_neelpati/Kaun_Banega_Neelpati.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package kaun_banega_neelpati;
+
+/**
+ *
+ * @author Vishal
+ */
+public class Kaun_Banega_Neelpati {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
