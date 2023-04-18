7110e784ea7ac22e8f86342a05cc5992705919d8
Vishal Pattanaik <vishal.pattanaik05@gmail.com>
2023-04-18 22:52:21 +0530

    sql registration successfull

--- src/register_page.java
+++ src/register_page.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -17,13 +14,16 @@ import javax.swing.UIManager;
 import javax.swing.UIManager.LookAndFeelInfo;
 import javax.swing.UnsupportedLookAndFeelException;
 
+import java.text.DateFormat;
+import java.util.Date;
+import java.text.SimpleDateFormat;  
 /**
  *
  * @author Vishal
  */
-public class login_page extends javax.swing.JFrame {
+public class register_page extends javax.swing.JFrame {
 
-    public login_page() {
+    public register_page() {
         initComponents();
     }
 
@@ -31,51 +31,42 @@ public class login_page extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        To_register = new javax.swing.JLabel();
-        Login_text = new javax.swing.JLabel();
+        Error_m = new javax.swing.JDialog();
+        register_text = new javax.swing.JLabel();
         username = new javax.swing.JTextField();
         password = new javax.swing.JPasswordField();
-        login_button = new javax.swing.JButton();
+        register_button = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
         main_Background = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        To_register.setFont(new java.awt.Font("Segoe UI Black", 0, 18)); // NOI18N
-        To_register.setText("Register");
-        To_register.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                To_registerMouseClicked(evt);
-            }
-        });
-        getContentPane().add(To_register, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 440, 70, 40));
-
-        Login_text.setFont(new java.awt.Font("Segoe UI Black", 0, 18)); // NOI18N
-        Login_text.setText("LOGIN");
-        getContentPane().add(Login_text, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 170, 70, 40));
+        register_text.setFont(new java.awt.Font("Segoe UI Black", 0, 18)); // NOI18N
+        register_text.setText("REGISTER");
+        getContentPane().add(register_text, new org.netbeans.lib.awtextra.AbsoluteConstraints(640, 160, 100, 40));
 
         username.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 usernameActionPerformed(evt);
             }
         });
-        getContentPane().add(username, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 230, 220, 50));
+        getContentPane().add(username, new org.netbeans.lib.awtextra.AbsoluteConstraints(570, 230, 220, 50));
 
         password.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 passwordActionPerformed(evt);
             }
         });
-        getContentPane().add(password, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 300, 220, 50));
+        getContentPane().add(password, new org.netbeans.lib.awtextra.AbsoluteConstraints(570, 300, 220, 50));
 
-        login_button.setText("Login");
-        login_button.addActionListener(new java.awt.event.ActionListener() {
+        register_button.setText("Register");
+        register_button.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                login_buttonActionPerformed(evt);
+                register_buttonActionPerformed(evt);
             }
         });
-        getContentPane().add(login_button, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 380, 160, 40));
+        getContentPane().add(register_button, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 380, 160, 40));
 
         jLabel1.setOpaque(true);
         getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 110, 720, 410));
@@ -95,18 +86,31 @@ public class login_page extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_passwordActionPerformed
 
-    private void login_buttonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_login_buttonActionPerformed
+    private void register_buttonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_register_buttonActionPerformed
         String user = username.getText();
         char[] pass_char = password.getPassword();
         String pass = new String(pass_char);
         
-        String database_user = "root";
-        String database_pass = "rootroot";
-        String url = "jdbc:mysql://127.0.0.1:3306/server";
-        try { 
+        DateFormat dateFormat = new SimpleDateFormat("yyMMddHHmmss");
+        Date da= new Date();
+        String unique_num = dateFormat.format(da);
+        
+        if(username == null || pass == null)
+        {
+            
+        }
+            String database_user = "root";
+            String database_pass = "rootroot";
+            String url = "jdbc:mysql://127.0.0.1:3306/server";
+            String Query = "Insert into users values ('"+unique_num+"','"+user+"','"+pass+"')";
+        try {
+            
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection connect = DriverManager.getConnection(url, database_user, database_pass);
-           
+            Statement stmt = connect.createStatement();
+            stmt.executeUpdate(Query);
+            this.dispose();
+            new login_page().setVisible(true);
             
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(login_page.class.getName()).log(Level.SEVERE, null, ex);
@@ -116,13 +120,7 @@ public class login_page extends javax.swing.JFrame {
      
         
         
-    }//GEN-LAST:event_login_buttonActionPerformed
-
-    private void To_registerMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_To_registerMouseClicked
-        this.dispose();
-        new register_page().setVisible(true);
-
-    }//GEN-LAST:event_To_registerMouseClicked
+    }//GEN-LAST:event_register_buttonActionPerformed
 
     public static void main(String args[]) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
         
@@ -152,12 +150,12 @@ public class login_page extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel Login_text;
-    private javax.swing.JLabel To_register;
+    private javax.swing.JDialog Error_m;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JButton login_button;
     private javax.swing.JLabel main_Background;
     private javax.swing.JPasswordField password;
+    private javax.swing.JButton register_button;
+    private javax.swing.JLabel register_text;
     private javax.swing.JTextField username;
     // End of variables declaration//GEN-END:variables
 }
